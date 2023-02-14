3faaf415d19cac079a6bd975b06c85280f9a7e40
Edmund Home <edmund.home@sequra.es>
2023-02-14 02:23:23 +0100

    refactored code

--- src/train_model.py
+++ src/train_model.py
@@ -1,16 +1,15 @@
 # Script to train machine learning model.
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from ml.data import process_data
-from ml.model import train_model, compute_model_metrics, inference
+from src.ml.data import process_data
+from src.ml.model import train_model, compute_model_metrics, inference, save_model
 
-import pickle
 
+random_state=123
 
 data_path = 'data/census.csv'
-output_model_path = 'model/output/logistic.sav'
+output_model_path = 'src/output/logistic.sav'
 
 cat_features = [
     "workclass",
@@ -22,20 +21,21 @@ cat_features = [
     "sex",
     "native-country",
 ]
+label = "salary"
 
 
 def main():
 
-    data = pd.read_csv(data_path, sep=', ')
-    train, test = train_test_split(data, test_size=0.20)
+    data = pd.read_csv(data_path, sep=', ', engine='python')
+    train, test = train_test_split(data, test_size=0.20, random_state=random_state)
 
     X_train, y_train, encoder, lb = process_data(
-        train, categorical_features=cat_features, label="salary", training=True
+        train, categorical_features=cat_features, label=label, training=True
     )
     trained_model = train_model(X_train, y_train)
-    print(type(trained_model))
+
     X_test, y_test, _, _ = process_data(
-        test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+        test, categorical_features=cat_features, label=label, training=False, encoder=encoder, lb=lb
     )
     precision, recall, fbeta = compute_model_metrics(y=y_test, preds=inference(trained_model, X_test))
 
@@ -43,7 +43,8 @@ def main():
     print("recall: ", recall)
     print("fbeta: ", fbeta)
 
-    pickle.dump(trained_model, open(output_model_path, 'wb'))
+    save_model(trained_model, output_model_path)
+
 
 
 if __name__ == "__main__":
