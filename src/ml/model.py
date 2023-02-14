076f88249edd166fe4a52b1591d9f5913f63c41f
Edmund Home <edmund.home@sequra.es>
2023-02-14 21:13:10 +0100

    added model slice performance

--- src/ml/model.py
+++ src/ml/model.py
@@ -2,6 +2,8 @@ from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.linear_model import LogisticRegression
 import pickle
 
+from src.ml.data import process_data
+
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -97,8 +99,37 @@ def load_model(path, return_encoder_and_lbl_binarizer=False):
             indicator to have the encoder also returned
 
     Returns:
-        A LogisticRegression model | (LogisticRegression model, encoder)
+        A LogisticRegression model | (LogisticRegression model, encoder, label_binarizer)
     """
     if not return_encoder_and_lbl_binarizer:
         return pickle.load(open(path+'logistic.sav', 'rb'))
     return pickle.load(open(path+'logistic.sav', 'rb')), pickle.load(open(path+'encoder.sav', 'rb')), pickle.load(open(path+'lb.sav', 'rb'))
+
+
+def model_slice_performance(model, test, feature_to_slice_on, cat_features, label, encoder, lb):
+    """generate performance metrics for every unique value in the feature to slice on
+
+    Args:
+        model (LogisticRegression): The model to test performance on
+        test (pd.Dataframe): the data to use for perfomance test on
+        feature_to_slice_on (str): the feature to use as a slice
+        cat_features (list): all categorical features the model uses
+        label (str): the label of the model
+        encoder (OneHotEncoder): the encoder of the model
+        lb (LabelBinarizer): the label binarizer
+    """
+    
+    print(f"Performance for {feature_to_slice_on}: ")
+    for cls in test[feature_to_slice_on].unique():
+
+        X_test, y_test, _, _ = process_data(
+            test[test[feature_to_slice_on] == cls], categorical_features=cat_features, label=label, training=False, encoder=encoder, lb=lb
+        )
+
+        precision, recall, fbeta = compute_model_metrics(y=y_test, preds=inference(model, X_test))
+
+        print(f"  {cls}:")
+
+        print("\tprecision: ", precision)
+        print("\trecall: ", recall)
+        print("\tfbeta: ", fbeta)
