028b86b4f4600dc199a159a85ee378244018404c
Your Name <you@example.com>
2023-02-25 17:25:45 +0100

    added is high earner post test

--- tests/test_main.py
+++ tests/test_main.py
@@ -24,9 +24,32 @@ def test_post_fail():
     assert r.status_code == 422
 
 
-def test_post_success():
+def test_post_is_not_high_earner():
     data = User.Config.schema_extra['example']
     
     r = client.post("/predict/", content=json.dumps(data))
     assert r.status_code == 200
     assert r.json() == False
+
+
+def test_post_is_high_earner():
+    data = {
+        "age": 52,
+        "workclass": "Self-emp-not-inc",
+        "fnlght": 209642,
+        "education": "HS-grad",
+        "education-num": 9,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 5178,
+        "capital-loss": 0,
+        "hours-per-week": 45,
+        "native-country": "United-States"
+    }
+    
+    r = client.post("/predict/", content=json.dumps(data))
+    assert r.status_code == 200
+    assert r.json() == True
